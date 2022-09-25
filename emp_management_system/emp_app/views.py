202aa64b71cf8d3423052cfce1e5c966e7c5c6e9
Ritesh2602 <riteshtahilramani1@gmail.com>
2022-09-25 15:22:00 +0530

    Successfully made View Employees page

--- emp_management_system/emp_app/views.py
+++ emp_management_system/emp_app/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
+from .models import Department,Role,Employee
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
 
 def view_emp(request):
-    return render(request, 'view_emp.html')
+    emp=Employee.objects.all()
+    params={
+        'emps':emp
+    }
+    print(params)
+    return render(request, 'view_emp.html',params)
 
 def add_emp(request):
     return render(request, 'add_emp.html')
