cb98aa0e692ab2ca3e96285710c17e3597d337a7
Ritesh2602 <riteshtahilramani1@gmail.com>
2022-09-25 20:17:20 +0530

    Done with Employee Management System

--- emp_management_system/emp_management_system/settings.py
+++ emp_management_system/emp_management_system/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-9xg&i6z3ylr86k-)p$kuq7dn+q3$545_g@p#awcxd(#v2yel4_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
